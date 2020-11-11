9193f0e84bb50430622254f44027c308bd263970
Avijeet Singh <avitomar99@gmail.com>
2020-11-11 09:35:01 -0800

    commit bulk config as well

---
+++ bulk_config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    USERNAME: 'xxxx',
+    PASSWORD: 'yyy',
+    FILENAME: 'orderextract4700todelete.csv',
+    FILETYPE: 'CSV',
+    SOBJECT: 'Order',
+    OPERATION: 'query',
+    //EXTERNALIDFIELDNAME : 'ECCOrderNumber__c',
+    LINEENDING : "LF",
+    APIVERSION: '48.0',
+    LOGINURL: 'https://test.salesforce.com/services/Soap/u/'
+}
